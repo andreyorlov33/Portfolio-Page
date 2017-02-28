cea82ded15ef282030fd359aa0cec7542df3e664
Andrey <andreyorlov33@gmail.com>
2017-02-27 19:18:30 -0800

    outlined the landing page, slide out set up

---
+++ assets/app.js
@@ -0,0 +1,30 @@
+ var slideout = new Slideout({
+                'panel': document.getElementById('panel'),
+                'menu': document.getElementById('menu'),
+                'side': 'right',
+                'padding': 256,
+                'tolerance': 70
+            });
+
+            document.querySelector('.toggle-button').addEventListener('click', function() {
+                slideout.toggle();
+
+            function close(eve) {
+                eve.preventDefault();
+                slideout.close();
+            }
+
+            slideout
+                .on('beforeopen', function() {
+                    this.panel.classList.add('panel-open');
+                })
+                .on('open', function() {
+                    this.panel.addEventListener('click', close);
+                })
+                .on('beforeclose', function() {
+                    this.panel.classList.remove('panel-open');
+                    this.panel.removeEventListener('click', close);
+                });
+        });
+
+
