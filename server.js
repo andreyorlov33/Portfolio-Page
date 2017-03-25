7f1d0600ee039905358e444c1188f400f9e92105
Andrey <andreyorlov33@gmail.com>
2017-03-25 01:09:40 -0700

    node mailer installed

--- server.js
+++ server.js
@@ -1,12 +1,49 @@
 // ******************************************************************************
 // *** Dependencies
 // =============================================================
-const express = require("express");
-const path = require('path');
+const express = require("express"),
+      path = require('path'),
+      bodyParser = require('body-parser'),
+      fs = require('fs'),
+      nodemailer = require('nodemailer'),
+      app = express(),
+      PORT = process.env.PORT || 8080;
 
-// Sets up the Express App
-const app = express();
-const PORT = process.env.PORT || 8080;
+
+
+
+// bodyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
+
+let transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'andreyorlov33',
+        pass: 'acscvb04'
+    }
+});
+
+app.post('/mail', (req, res) => {
+    let mailOptions = {
+        to: 'andreyorlov33@gmail.com',
+        subject: `Email from your website ${req.body.email}`,
+        text: req.body.message
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log(error);
+        }else{
+            console.log('Message sent: ' + info.response);
+            res.redirect('/contact')
+        };
+    });
+});
 
 
 // Static directory
@@ -19,6 +56,11 @@ app.get('/', (request,response)=>{
 app.get('/projects', (request,response)=>{
     response.sendFile(path.join(__dirname, `./public/projects.html`))
 })
+
+app.get('/contact', (request,response)=>{
+    response.sendFile(path.join(__dirname, `./public/contact.html`))
+})
+
 
 
 app.get('/resume', (request,response)=>{
